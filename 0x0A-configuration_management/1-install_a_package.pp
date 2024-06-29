11d52f0edf8a294037c67e3f2e6b571490941c3f
YusufYuso <yusuf.elsaed@gmail.com>
2024-06-29 05:09:23 +0300

    Updating ASCII text file: 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Using Puppet, install flask from pip3.
 
-exec { 'install flask':
-  command =>  'pip3 install flask==2.1.0',
-  path    =>  '/usr/bin/',
-  unless  =>  'pip3 list | grep flask',
+package { 'flask':
+  ensure   =>  '2.1.0',
+  provider => 'pip3',
 }
