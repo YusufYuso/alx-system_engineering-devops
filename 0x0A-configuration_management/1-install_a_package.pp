b98bf306a8736724fb4aef834debbb456912b968
YusufYuso <yusuf.elsaed@gmail.com>
2024-07-02 05:21:16 +0300

    Updating ASCII text file: 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
 # Using Puppet, install flask from pip3.
-package { 'flask':
+
+package { 'python3.8.10':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
